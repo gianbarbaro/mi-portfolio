d2cad32971893541077d3edce56e123bca123b17
gianbarbaro <gianlbarbaro@gmail.com>
2023-02-21 20:24:09 -0300

    first commit

---
+++ src/main/java/com/gianlucabarbaro/GianlucabarbaroApplication.java
@@ -0,0 +1,13 @@
+package com.gianlucabarbaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GianlucabarbaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GianlucabarbaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gianlucabarbaro/GianlucabarbaroApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.gianlucabarbaro;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GianlucabarbaroApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
